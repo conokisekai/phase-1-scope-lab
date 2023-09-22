1dff0b65895248affea9e8ee32781fb654c4088d
Conrad Kambi <kambiconrad@gmail.com>
2023-09-22 14:26:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+// Initialize global variables
+window.customerName = 'bob';
+window.bestCustomer;
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  try {
+    const leastFavoriteCustomer = 'initial value';
+    leastFavoriteCustomer = 'new value';
+  } catch (error) {
+    throw new Error('Assignment to constant variable.');
+  }
+}
